3b850ad947708b66fd9e57994e4cbf42cd3b6b39
Fadel Mamar Seydou <fadel.seydou@epfl.ch>
2024-06-02 22:52:53 +0100

    adding arguments

--- src/arguments.py
+++ src/arguments.py
@@ -6,12 +6,15 @@ class Arguments:
 
     # data preparation arguments
     root_path:str = r'C:\Users\fadel'
-    dest_path:str='../data/train/tiles_images'
+    dest_path_images:str='../data/train/images'
+    dest_path_labels:str='../data/train/labels'
     height:int = 512
     width:int = 512
-    overlap:int=50
+    overlap:int=80
     min_visibility:float=0.0
     save_all:bool=False
 
     # model type
     is_detector:bool=True
+    
+    # training hyperparameters
