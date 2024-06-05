c100553a9c0d5c88864bde15100901db54836cfa
fadel <fadel.seydou@gmail.com>
2024-06-06 00:37:59 +0200

    adding cd

--- run-labelstudio-windows.bat
+++ run-labelstudio-windows.bat
@@ -2,6 +2,8 @@
 call "C:\Users\Machine Learning\anaconda3\Scripts\activate.bat" "C:\Users\Machine Learning\anaconda3"
 call conda activate label-studio
 
+call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\my_ml_backend"
+
 :: call set LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=True
 :: call set LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT="D:\"
 
--- run-ml-backend-windows.bat
+++ run-ml-backend-windows.bat
@@ -2,10 +2,12 @@
 call "C:\Users\Machine Learning\anaconda3\Scripts\activate.bat" "C:\Users\Machine Learning\anaconda3"
 call conda activate label-backend
 
+call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\my_ml_backend"
+
 :: call set LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=True
 :: call set LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT="D:\"
 
-label-studio-ml start ./my_ml_backend -p 9090
+label-studio-ml start ../my_ml_backend -p 9090
 
 :: deactivate
 call "C:\Users\Machine Learning\Desktop\workspace-wildAI\envs\label-studio\Scripts\deactivate.bat"
