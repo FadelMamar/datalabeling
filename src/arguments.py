22777cbd3242ec69988886185c262e7a544a1f79
fadel <fadel.seydou@gmail.com>
2024-06-06 01:51:08 +0200

    adding new parameter

--- src/arguments.py
+++ src/arguments.py
@@ -8,8 +8,8 @@ class Arguments:
     root_path:str = r'C:\Users\fadel'
     dest_path_images:str='../data/train/images'
     dest_path_labels:str='../data/train/labels'
-    height:int = 512
-    width:int = 512
+    height:int = 640
+    width:int = 640
     overlap:int=80
     min_visibility:float=0.0
     save_all:bool=False
@@ -22,3 +22,6 @@ class Arguments:
 
     # training hyperparameters
     epochs:int=20
+
+    # labels to discard
+    discard_labels:tuple = ('other','rocks','vegetation','detection','termite mound','label')
