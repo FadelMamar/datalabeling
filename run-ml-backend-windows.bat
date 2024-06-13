c3952605bc02d50284b30e6d80717ffebac50d69
fadel <fadel.seydou@gmail.com>
2024-06-13 09:04:13 +0200

    fixing issues with root location

--- run-ml-backend-windows.bat
+++ run-ml-backend-windows.bat
@@ -2,9 +2,12 @@
 call "C:\Users\Machine Learning\anaconda3\Scripts\activate.bat" "C:\Users\Machine Learning\anaconda3"
 call conda activate label-backend
 
-call conda env config vars set LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT="D:\"
+call conda env config vars set LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT=D:\
 call conda env config vars set LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED="True"
-call conda env config vars set LOCAL_FILES_DOCUMENT_ROOT="D:\"
+call conda env config vars set LOCAL_FILES_DOCUMENT_ROOT=D:\
+
+call conda deactivate
+call conda activate label-studio
 
 call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\my_ml_backend"
 
