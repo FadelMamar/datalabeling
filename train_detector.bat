e83e05df92a1479a4d2743aff76f3912892ac36e
fadel <fadel.seydou@gmail.com>
2024-07-08 18:44:21 +0200

    update

--- train_detector.bat
+++ train_detector.bat
@@ -6,11 +6,16 @@ call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
 
 call wandb online
 
-call python ".\src\train.py" --batchsize 32  --epochs 50 --lr0 0.0001 --patience 50 --is-detector^
+:: freeze layers for transfer-learning
+
+call python ".\src\train.py" --batchsize 32  --epochs 50 --lr0 0.0001 --lrf 0.1 --patience 10 --is-detector^
     --data-config-yaml "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\data_config.yaml" ^
     --path-weights "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\base_models_weights\yolov8.kaza.pt" ^
-    --run-name "detector" ^
-    --dest-path-images "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train\images"
+    --run-name "detector" --project-name "wildAI-detection"^
+    --dest-path-images "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train\images"^
+    --tag "dataset-v2"^ 
+    --weightdecay 0.0005
+:: --freeze 22
 
 
 call conda deactivate
--- train_identificator.bat
+++ train_identificator.bat
@@ -3,26 +3,14 @@ call conda activate label-backend
 
 call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
 
-:: --cos-lr 
+
 call wandb online
 
-call python ".\src\train.py" --batchsize 32  --epochs 100 --lr0 0.001 --patience 50^
+call python ".\src\train.py" --batchsize 32  --epochs 50 --lr0 0.0001 --lrf 0.01 --patience 50^
     --data-config-yaml "D:\PhD\Data per camp\IdentificationDataset\data_config.yaml" ^
     --path-weights "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\base_models_weights\yolov8.kaza.pt" ^
     --run-name "identificator" ^
-    --dest-path-images "D:\PhD\Data per camp\IdentificationDataset\train\images"
-
-call python ".\src\train.py" --batchsize 32  --epochs 100 --lr0 0.001 --patience 50^
-    --data-config-yaml "D:\PhD\Data per camp\IdentificationDataset\data_config.yaml" ^
-    --path-weights "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\base_models_weights\yolov8m.pt" ^
-    --run-name "identificator" ^
-    --dest-path-images "D:\PhD\Data per camp\IdentificationDataset\train\images"
-
-call python ".\src\train.py" --batchsize 32  --epochs 100 --lr0 0.001 --patience 50^
-    --data-config-yaml "D:\PhD\Data per camp\IdentificationDataset\data_config.yaml" ^
-    --path-weights "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\base_models_weights\yolov8s.pt" ^
-    --run-name "identificator" ^
-    --dest-path-images "D:\PhD\Data per camp\IdentificationDataset\train\images"
-
-
+    --dest-path-images "D:\PhD\Data per camp\IdentificationDataset\train\images"^
+    --tag "yolov8-kaza"
+:: --cos-lr 
 call conda deactivate
