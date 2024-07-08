b9e8aafbc4aefa2ec77f2cd17a4f4575d2ebcf5d
fadel <fadel.seydou@gmail.com>
2024-07-08 18:42:39 +0200

    adding details on arguments

--- build_yolo_dataset.bat
+++ build_yolo_dataset.bat
@@ -5,6 +5,11 @@ call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\src"
 
 :: add --clear-yolo-dir to clear data in ./data/train
 
-call python cli.py --build-yolo-dataset --clear-yolo-dir --empty-ratio 1 --is-detector
+call python cli.py --build-yolo-dataset --clear-yolo-dir --empty-ratio 1 --is-detector^
+    --dest-path-labels "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train\labels" ^
+    --dest-path-images "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train\images" ^
+    --coco-json-dir "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\exported_annotations\coco-format" ^
+    --ls-json-dir "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\exported_annotations\json" ^
+    --data-config-yaml "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\data_config.yaml"
 
 call conda deactivate
