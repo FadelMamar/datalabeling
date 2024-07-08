ae4c7b089e6ac583fcb7edc3ccb0f884ad78e013
fadel <fadel.seydou@gmail.com>
2024-07-08 18:44:40 +0200

    adding sweeps for hyperparameter search

---
+++ wandb_sweep.bat
@@ -0,0 +1,11 @@
+call "C:\Users\Machine Learning\anaconda3\Scripts\activate.bat" "C:\Users\Machine Learning\anaconda3"
+call conda activate label-backend
+
+call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
+
+
+call wandb online
+
+call python ".\src\sweeps.py"
+
+call conda deactivate
