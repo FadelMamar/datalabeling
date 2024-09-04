2558bbf44a78b1cd6a6eb8fc52cfdf5afc2aa1e5
fadel <fadel.seydou@gmail.com>
2024-09-04 17:07:16 +0200

    renaming

--- helper-scripts/build_detector_dataset.bat
+++ helper-scripts/build_detector_dataset.bat
@@ -5,9 +5,9 @@ call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\tools"
 
 :: add --clear-yolo-dir to clear data in ./data/train
 
-call python cli.py --build-yolo-dataset --clear-yolo-dir --empty-ratio 1 --is-detector^
-    --dest-path-labels "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train\labels" ^
-    --dest-path-images "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train\images" ^
+call python cli.py --build-yolo-dataset --clear-yolo-dir --empty-ratio 0.1 --is-detector^
+    --dest-path-labels "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train-wildai\labels" ^
+    --dest-path-images "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\train-wildai\images" ^
     --coco-json-dir "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\exported_annotations\coco-format" ^
     --ls-json-dir "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\exported_annotations\json" ^
     --data-config-yaml "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\data\data_config.yaml"
