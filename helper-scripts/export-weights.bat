85394be4f808a83721e082c3258e6c681c8ca979
fadel <fadel.seydou@gmail.com>
2024-11-15 21:44:50 +0100

    adding args

--- helper-scripts/export-weights.bat
+++ helper-scripts/export-weights.bat
@@ -2,8 +2,8 @@ call .\activate_label-backend_env.bat
 
 call cd ..
 
-call python tools\cli.py --export-format "openvino" --width 640 --height 640 --export-batch-size 1^
+call python tools\cli.py --export-format "openvino" --width 1280 --height 1280 --export-batch-size 1^
              --export-model-weights "C:\Users\FADELCO\OneDrive\Bureau\datalabeling\models\best.pt"^
-             --half
+             --half --dynamic
 
 call conda deactivate
