3b605fc2e52718fcdb86a7d53982d5ecac617850
fadel <fadel.seydou@gmail.com>
2024-11-17 14:53:08 +0000

    update

--- tools/sample_hard_negatives.py
+++ tools/sample_hard_negatives.py
@@ -1,43 +1,72 @@
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
-from datalabeling.dataset.sampling import (compute_predictions, 
-                                            load_prediciton_results,
-                                            load_groundtruth, 
-                                            compute_detector_performance,
-                                            select_hard_samples)
+from datalabeling.annotator import Detector
+from datalabeling.dataset.sampling import (get_preds_targets, 
+                                           select_hard_samples,
+                                           compute_detector_performance)
+import pandas as pd
+import yaml
+import os
+from dataclasses import dataclass
+from datargs import parse
 
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@dataclass
+class Args:
+
+    path_to_weights:str=r"C:\Users\FADELCO\OneDrive\Bureau\datalabeling\models\best_openvino_model"
+    confidence_threshold:float=0.1
+    overlap_ratio:float=0.1
+    tilesize:int=1280
+    device:str="cpu"
+    is_yolo_obb:bool=False
+
+    data_config_yaml:str
+    split:str="train"
+
+    pred_results_dir:str=os.path.join(CUR_DIR,r"..\data")
+    load_results:bool=False
+
+    map_thrs:float=0.3
+    score_thrs:float=0.7
+    save_path_samples:str=r"D:\hard_negative_samples\hard_samples.txt"
+    data_config_root:str="D:\\"
+    save_data_config_yaml:str= os.path.join(CUR_DIR,r"..\data\hard_samples.yaml")
 
 
 if __name__ == "__main__":
 
-    # compute predictions
-    path_to_results = compute_predictions(weight_path=r"C:\Users\FADELCO\OneDrive\Bureau\datalabeling\models\best_openvino_model",
-                                        task='obb',
-                                        iou=0.6,
-                                        half=True,
-                                        imgsz=1280,
-                                        results_dir_name=r"D:\validation_results",
-                                        val_run_name='hard_samples',
-                                        data_config_yaml=r"C:\Users\FADELCO\OneDrive\Bureau\datalabeling\data\data_config.yaml",
-                                        batch_size=1)
+    args = parse(Args)
 
-    # load prediction results
-    df_results = load_prediciton_results(path_to_results)
+    # Define detector
+    model = Detector(path_to_weights=args.path_to_weights,
+                    confidence_threshold=args.confidence_threshold,
+                    overlap_ratio=args.overlap_ratio,
+                    tilesize=args.tilesize,
+                    device=args.device,
+                    is_yolo_obb=args.is_yolo_obb)
 
-    # load gt
-    df_labels, col_names = load_groundtruth(data_config_yaml=r"C:\Users\FADELCO\OneDrive\Bureau\datalabeling\data\data_config.yaml")
+    # load groundtruth
+    with open(args.data_config_yaml,'r') as file:
+        yolo_config = yaml.load(file,Loader=yaml.FullLoader)
 
-    # compute mAP
-    df_results_per_img = compute_detector_performance(df_results=df_results,
-                                                    df_labels=df_labels,
-                                                    col_names=col_names)
+    images_path = [os.path.join(yolo_config['path'],yolo_config[args.split][i]) for i in range(len(yolo_config[args.split]))]
+    labels_path = [p.replace('images','labels') for p in images_path]
+
+    df_results, df_labels, col_names = get_preds_targets(images_dirs=images_path,
+                                                    pred_results_dir=args.pred_results_dir,
+                                                    detector=model,
+                                                    load_results=args.load_results)
+
+    df_results_per_img = compute_detector_performance(df_results,df_labels,col_names)
 
     # save hard samples
     df_hard_negatives = select_hard_samples(df_results_per_img=df_results_per_img,
-                                            map_thrs=0.3,
-                                            score_thrs=0.7,
-                                            save_path_samples=r"D:\hard_negative_samples\hard_samples.txt",
-                                            root='D:\\',
-                                            save_data_yaml=r"C:\Users\FADELCO\OneDrive\Bureau\datalabeling\data\hard_samples.yaml"
+                                                map_thrs=args.map_thrs,
+                                                score_thrs=args.score_thrs,
+                                                save_path_samples=args.save_path_samples,
+                                                root=args.data_config_root,
+                                                save_data_yaml=args.save_data_config_yaml
                                             )
