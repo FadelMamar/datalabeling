d9339e3435d92002804bbfd65f0e96cc664fd84a
fadel <fadel.seydou@gmail.com>
2025-03-14 13:18:12 +0100

    update

--- helper-scripts/register_run_to_mlflow.bat
+++ helper-scripts/register_run_to_mlflow.bat
@@ -8,7 +8,7 @@ call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
 
 
 call uv run tools\register_model.py --exp-name "labeler" --model ""^
-        --model-name "obb-detector"^
+        --model-name "obb-detector" --mlflow-tracking-uri "http://localhost:5000"^
         --tilesize 1280 --imgsz 1280 --confidence-threshold 0.1 --overlap-ratio 0.15^
         --is-yolo-obb --use-sliding-window
 
