11af3547771eedd1fa504289e9477301e009b7fb
fadel <fadel.seydou@gmail.com>
2025-03-15 12:07:13 +0200

    pulling commit

--- helper-scripts/build_yolo_seg_dataset.bat
+++ helper-scripts/build_yolo_seg_dataset.bat
@@ -1,9 +1,9 @@
 call .\activate_label-backend_env.bat
 
-call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\tools"
+call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
 
 @REM :: add 
 
 @REM :: Example builds detection dataset
-call uv run  build_dataset.py --obb-to-yolo --skip --create-yolo-seg-dir --copy-images --data-config-yaml ""^
-    --sam-model-path "./sam2.1_l.pt"
+call uv run  tools\build_dataset.py --obb-to-yolo --skip --create-yolo-seg-dir --copy-images --data-config-yaml "data\dataset_identification-detection.yaml"^
+    --sam-model-path "base_models_weights\sam2.1_l.pt"
