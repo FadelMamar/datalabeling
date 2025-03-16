4fc382aa6849fbdddfa1bce72dc67ad2c1cc4a04
fadel <fadel.seydou@gmail.com>
2025-03-16 21:53:34 +0100

    update

--- tools/paddlepaddle.py
+++ tools/paddlepaddle.py
@@ -50,7 +50,6 @@ class Flags:
 
     # training
     resume:bool=False
-    eval:bool=False
 
     # logging
     output_dir:str=None
@@ -74,4 +73,80 @@ def train_ppd(args:Flags):
     mlflow.set_experiment(args.project_name)
     mlflow.paddle.autolog(log_every_n_epoch = 5)
     with mlflow.start_run(run_name=args.run_name) as run:
-        trainer.train(flags.eval)
+        trainer.train(args.do_eval)
+
+
+def get_test_images(infer_dir:str, infer_img:str, infer_list=None):
+    """
+    Get image path list in TEST mode
+    """
+    assert infer_img is not None or infer_dir is not None, \
+        "--infer_img or --infer_dir should be set"
+    assert infer_img is None or os.path.isfile(infer_img), \
+            "{} is not a file".format(infer_img)
+    assert infer_dir is None or os.path.isdir(infer_dir), \
+            "{} is not a directory".format(infer_dir)
+
+    # infer_img has a higher priority
+    if infer_img and os.path.isfile(infer_img):
+        return [infer_img]
+
+    images = set()
+    infer_dir = os.path.abspath(infer_dir)
+    assert os.path.isdir(infer_dir), \
+        "infer_dir {} is not a directory".format(infer_dir)
+    if infer_list:
+        assert os.path.isfile(
+            infer_list), f"infer_list {infer_list} is not a valid file path."
+        with open(infer_list, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            images.update([os.path.join(infer_dir, line.strip())])
+    else:
+        exts = ['jpg', 'jpeg', 'png', 'bmp']
+        exts += [ext.upper() for ext in exts]
+        for ext in exts:
+            images.update(glob.glob('{}/*.{}'.format(infer_dir, ext)))
+    images = list(images)
+    assert len(images) > 0, "no image found in {}".format(infer_dir)
+    logger.info("Found {} inference images in total.".format(len(images)))
+
+    return images
+
+
+def inference(images,args:Flags):
+
+    cfg = load_config(args.config)
+
+    if args.rtn_im_file:
+        cfg['TestReader']['sample_transforms'][0]['Decode'][
+            'rtn_im_file'] = args.rtn_im_file
+    ssod_method = cfg.get('ssod_method', None)
+    if ssod_method == 'ARSL':
+        trainer = Trainer_ARSL(cfg, mode='test')
+        trainer.load_weights(args.weights, ARSL_eval=True)
+    else:
+        trainer = Trainer(cfg, mode='test')
+        trainer.load_weights(args.weights)
+    
+    if args.slice_infer:
+        trainer.slice_predict(
+            images,
+            slice_size=args.slice_size,
+            overlap_ratio=args.overlap_ratio,
+            combine_method=args.combine_method,
+            match_threshold=args.match_threshold,
+            match_metric=args.match_metric,
+            draw_threshold=args.draw_threshold,
+            output_dir=args.output_dir,
+            save_results=args.save_results,
+            visualize=args.visualize)
+    else:
+        trainer.predict(
+            images,
+            draw_threshold=args.draw_threshold,
+            output_dir=args.output_dir,
+            save_results=args.save_results,
+            visualize=args.visualize,
+            save_threshold=args.save_threshold,
+            do_eval=args.do_eval)
