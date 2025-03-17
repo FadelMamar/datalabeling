54196dc93d0c782f0444761f3b07f5515a757b99
fadel <fadel.seydou@gmail.com>
2025-03-17 13:51:41 +0200

    chaining script

---
+++ helper-scripts/chaining.bat
@@ -0,0 +1,9 @@
+call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
+
+call helper-scripts\build_yolo_seg_dataset.bat
+
+call helper-scripts\train_identificator.bat
+
+call uv run tools\train-herdnet.py
+
+shutdown -s
