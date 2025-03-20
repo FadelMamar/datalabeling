5e7f07d10d0d00fada07c67603fb561775ab642a
fadel <fadel.seydou@gmail.com>
2025-03-20 16:44:14 +0200

    update

--- helper-scripts/convert_dataset_yolo_coco.bat
+++ helper-scripts/convert_dataset_yolo_coco.bat
@@ -1,12 +1,14 @@
-call .\activate_label-backend_env.bat
-
 call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
 
-:: yolo to obb
-call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --data-config-yaml "data\dataset_identification-detection.yaml" ^
-     --coco-output-dir "D:\PhD\Data per camp\DetectionDataset\Identification-split\coco-dataset" --skip
+call deactivate 
 
-call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --data-config-yaml "data\dataset_identification.yaml" ^
-    --coco-output-dir "D:\PhD\Data per camp\IdentificationDataset\coco-dataset" --skip
+call helper-scripts\activate_label-backend_env.bat
+
+:: yolo to obb
+@REM call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --data-config-yaml "data\dataset_identification-detection.yaml" ^
+@REM      --coco-output-dir "D:\PhD\Data per camp\DetectionDataset\Identification-split\coco-dataset" --skip
+
+@REM call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --data-config-yaml "data\dataset_identification.yaml" ^
+@REM     --coco-output-dir "D:\PhD\Data per camp\IdentificationDataset\coco-dataset" --skip
 
 @REM call deactivate
--- helper-scripts/launch_mlflow_server.bat
+++ helper-scripts/launch_mlflow_server.bat
@@ -1,5 +1,5 @@
-call .\activate_label-backend_env.bat
-
 call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
+
+call helper-scripts\activate_label-backend_env.bat
 
 call mlflow server --backend-store-uri runs\mlflow 
