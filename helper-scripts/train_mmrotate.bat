b993f581a56878b72cc1487660e05449c4309f9e
fadel <fadel.seydou@gmail.com>
2025-03-23 01:07:46 +0200

    update

--- helper-scripts/train_mmrotate.bat
+++ helper-scripts/train_mmrotate.bat
@@ -5,12 +5,54 @@ call deactivate
 
 call .venv-mmrotate\Scripts\activate.bat
 
-@REM call wandb online
+call wandb online
 
-call uv run tools\train-mmrotate.py --epoch 30 ^
-    --lr0 0.001 --config notebooks\oriented_rcnn_r50_fpn_1x_dota_le90.py ^
-    --weights notebooks\oriented_rcnn_r50_fpn_1x_dota_le90-6d2b2ce0.pth ^
-    --run-name "oriented_rcnn_r50_fpn" --output-dir runs-mmrotate\oriented_rcnn_r50_fpn ^
-    --tags "mmrotate" "detection" --empty-ratio 0.1 --data-config data\data_config.yaml
+@REM call uv run python -W once tools\train-mmrotate.py --epoch 30 --batch-size 8 --enable-val ^
+@REM     --lr0 0.001 --config "configs\mmrotate_configs\oriented_rcnn_r50_fpn_1x_dota_le90.py" ^
+@REM     --data-config "configs\yolo_configs\dataset_identification-detection.yaml" ^
+@REM     --project-name "wildAI-detection" --use-wandb ^
+@REM     --weights "base_models_weights\oriented_rcnn_r50_fpn_1x_dota_le90-6d2b2ce0.pth" ^
+@REM     --run-name "oriented_rcnn_r50_fpn" --output-dir runs-mmrotate\oriented_rcnn_r50_fpn ^
+@REM     --tags "mmrotate" "detection" --empty-ratio 0.1
 
-@REM call deactivate
+@REM call uv run python -W once tools\train-mmrotate.py --epoch 30 --batch-size 8 --enable-val ^
+@REM     --lr0 0.001 --config "configs\mmrotate_configs\redet_re50_refpn_1x_dota_le90.py" ^
+@REM     --data-config "configs\yolo_configs\dataset_identification-detection.yaml" ^
+@REM     --project-name "wildAI-detection" --use-wandb ^
+@REM     --weights "base_models_weights\redet_re50_fpn_1x_dota_ms_rr_le90-fc9217b5.pth" ^
+@REM     --run-name "redet_re50_fpn" --output-dir runs-mmrotate\redet_re50_fpn ^
+@REM     --tags "mmrotate" "detection" --empty-ratio 0.1
+
+@REM call uv run python -W once tools\train-mmrotate.py --epoch 30 --batch-size 16 --enable-val ^
+@REM     --lr0 0.001 --config "configs\mmrotate_configs\roi_trans_r50_fpn_1x_dota_ms_rr_le90.py" ^
+@REM     --data-config "configs\yolo_configs\dataset_identification-detection.yaml" ^
+@REM     --project-name "wildAI-detection" --use-wandb ^
+@REM     --weights "base_models_weights\roi_trans_r50_fpn_1x_dota_ms_rr_le90-fa99496f.pth" ^
+@REM     --run-name "roi_trans_r50_fpn" --output-dir runs-mmrotate\roi_trans_r50_fpn ^
+@REM     --tags "mmrotate" "detection" --empty-ratio 0.1
+
+@REM call uv run python -W once tools\train-mmrotate.py --epoch 10 --batch-size 16 --enable-val ^
+@REM     --lr0 0.001 --config "configs\mmrotate_configs\oriented_rcnn_r50_fpn_1x_dota_le90.py" ^
+@REM     --data-config "configs\yolo_configs\dataset_identification-detection.yaml" ^
+@REM     --project-name "wildAI-detection" --use-wandb ^
+@REM     --weights "runs-mmrotate\oriented_rcnn_r50_fpn\best_mAP_epoch_13.pth" ^
+@REM     --run-name "oriented_rcnn_r50_fpn" --output-dir runs-mmrotate\oriented_rcnn_r50_fpn ^
+@REM     --tags "mmrotate" "detection" --empty-ratio 2.5 --freeze-ratio 0.5
+
+@REM call uv run python -W once tools\train-mmrotate.py --epoch 10 --batch-size 16 --enable-val ^
+@REM     --lr0 0.0005 --config "configs\mmrotate_configs\redet_re50_refpn_1x_dota_le90.py" ^
+@REM     --data-config "configs\yolo_configs\dataset_identification-detection.yaml" ^
+@REM     --project-name "wildAI-detection" --use-wandb ^
+@REM     --weights "runs-mmrotate\redet_re50_fpn\latest.pth" ^
+@REM     --run-name "redet_re50_fpn" --output-dir runs-mmrotate\redet_re50_fpn ^
+@REM     --tags "mmrotate" "detection" --empty-ratio 2.5 --freeze-ratio 0.5
+
+@REM call uv run python -W once tools\train-mmrotate.py --epoch 10 --batch-size 16 --enable-val ^
+@REM     --lr0 0.0005 --config "configs\mmrotate_configs\roi_trans_r50_fpn_1x_dota_ms_rr_le90.py" ^
+@REM     --data-config "configs\yolo_configs\dataset_identification-detection.yaml" ^
+@REM     --project-name "wildAI-detection" --use-wandb ^
+@REM     --weights "runs-mmrotate\roi_trans_r50_fpn\latest.pth" ^
+@REM     --run-name "roi_trans_r50_fpn" --output-dir runs-mmrotate\roi_trans_r50_fpn ^
+@REM     --tags "mmrotate" "detection" --empty-ratio 2.5 --freeze-ratio 0.5
+
+call deactivate
--- helper-scripts/train_ppd.bat
+++ helper-scripts/train_ppd.bat
@@ -5,10 +5,11 @@ call deactivate
 
 call .venv-paddle\Scripts\activate.bat
 
-@REM call wandb online
+call wandb online
 
-call uv run tools\train_paddlepaddle.py --epoch 30 --lr0 0.0005 --amp --output-dir "runs_ppd\ppyoloe_plus_sod_crn_s_80e_visdrone" --use-wandb --project-name "wildAI-detection"^
-                                  --run-name "run-ppd" --config "configs\ppd_configs\ppyoloe_plus_sod_crn_s_80e_visdrone.yml" --do-eval True^
+call uv run tools\train_paddlepaddle.py --epoch 30 --lr0 0.0005 --amp  --use-wandb --project-name "wildAI-detection"^
+                                  --do-eval True --eval-interval 2 --output-dir "runs_ppd\ppyoloe_plus_sod_crn_s_80e_visdrone" ^
+                                  --run-name "ppyoloe_plus_sod_crn_s_80e_visdrone" --config "configs\ppd_configs\ppyoloe_plus_sod_crn_s_80e_visdrone.yml" ^
                                   --weights "base_models_weights\ppyoloe_plus_sod_crn_s_80e_visdrone.pdparams"  --tags "debug"
 
 @REM call deactivate
