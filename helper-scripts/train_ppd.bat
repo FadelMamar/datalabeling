3a847bf927d20db63b605bdc55a0c79aa14c748b
fadel <fadel.seydou@gmail.com>
2025-03-23 14:06:55 +0100

    update

--- helper-scripts/train_ppd.bat
+++ helper-scripts/train_ppd.bat
@@ -3,12 +3,13 @@ call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
 
 call deactivate
 
-call .venv-paddle\Scripts\activate.bat
+call D:\PaddleDetection\.venv\Scripts\activate
 
-@REM call wandb online
+call wandb offline
 
-call uv run tools\train_paddlepaddle.py --epoch 30 --lr0 0.0005 --amp --output-dir "runs_ppd\ppyoloe_plus_sod_crn_s_80e_visdrone" --use-wandb --project-name "wildAI-detection"^
+call uv run tools\train_paddlepaddle.py --epoch 30 --lr0 0.001 --amp --output-dir "runs_ppd\ppyoloe_plus_sod_crn_s_80e_visdrone" --use-wandb --project-name "wildAI-detection"^
                                   --run-name "run-ppd" --config "configs\ppd_configs\ppyoloe_plus_sod_crn_s_80e_visdrone.yml" --do-eval True^
-                                  --weights "base_models_weights\ppyoloe_plus_sod_crn_s_80e_visdrone.pdparams"  --tags "debug"
+                                  --weights "base_models_weights\ppyoloe_plus_sod_crn_s_80e_visdrone.pdparams"  --tags "debug" --device "cpu"^
+                                  --data-config "configs\yolo_configs\data_config.yaml" --tr-empty-ratio 0.1 --val-empty-ratio 0.3 --print-flops
 
 @REM call deactivate
