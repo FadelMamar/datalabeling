b82ed6a36d15f31b204be99dc16f76ed8ff660ad
fadel <fadel.seydou@gmail.com>
2025-03-23 22:55:17 +0200

    update

--- helper-scripts/convert_dataset_yolo_coco.bat
+++ helper-scripts/convert_dataset_yolo_coco.bat
@@ -5,10 +5,10 @@ call deactivate
 call helper-scripts\activate_label-backend_env.bat
 
 :: yolo to obb
-@REM call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --data-config-yaml "data\dataset_identification-detection.yaml" ^
-@REM      --coco-output-dir "D:\PhD\Data per camp\DetectionDataset\Identification-split\coco-dataset" --skip --clear-coco_dir
+@REM call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --clear-coco-dir --data-config-yaml "configs\yolo_configs\dataset_identification-detection.yaml" ^
+@REM      --coco-output-dir "D:\PhD\Data per camp\DetectionDataset\Identification-split\coco-dataset" --skip 
 
-@REM call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --data-config-yaml "data\dataset_identification.yaml" ^
-@REM     --coco-output-dir "D:\PhD\Data per camp\IdentificationDataset\coco-dataset" --skip --clear-coco_dir
+@REM call python  tools\build_dataset.py --obb-to-yolo --yolo-to-coco --data-config-yaml "configs\yolo_configs\dataset_identification.yaml" ^
+@REM     --coco-output-dir "D:\PhD\Data per camp\IdentificationDataset\coco-dataset" --skip --clear-coco-dir
 
 @REM call deactivate
