fixing computation of test metrics using ultralytics

tools/validate.py
@@ -2,6 +2,12 @@ from datargs import parse
 from dataclasses import dataclass
 from typing import Sequence
 import json
+from ultralytics.utils.metrics import ConfusionMatrix, DetMetrics, box_iou
+from ultralytics.data import converter
+from ultralytics.models.yolo.detect import DetectionValidator
+from pathlib import Path
+import seaborn
+import matplotlib.pyplot as plt
 
 @dataclass
 class Flags:
@@ -38,49 +44,107 @@ class Flags:
     model_type:str = "ultralytics" # ultralytics or HerdNet 
 
 
+
+class CustomValidator(DetectionValidator):
+    """From https://github.com/ultralytics/ultralytics/blob/main/ultralytics/models/yolo/detect/val.py
+    Adapted to compute confusion matrix for a given iou threshold
+    """
+
+    def init_metrics(self, model):
+        """
+        Initialize evaluation metrics for YOLO detection validation.
+
+        Args:
+            model (torch.nn.Module): Model to validate.
+        """
+        val = self.data.get(self.args.split, "")  # validation path
+        self.is_coco = (
+            isinstance(val, str)
+            and "coco" in val
+            and (val.endswith(f"{os.sep}val2017.txt") or val.endswith(f"{os.sep}test-dev2017.txt"))
+        )  # is COCO
+        self.is_lvis = isinstance(val, str) and "lvis" in val and not self.is_coco  # is LVIS
+        self.class_map = converter.coco80_to_coco91_class() if self.is_coco else list(range(1, len(model.names) + 1))
+        self.args.save_json |= self.args.val and (self.is_coco or self.is_lvis) and not self.training  # run final val
+        self.names = model.names
+        self.nc = len(model.names)
+        self.end2end = getattr(model, "end2end", False)
+        self.metrics.names = self.names
+        self.metrics.plot = self.args.plots
+        self.confusion_matrix = ConfusionMatrix(nc=self.nc, conf=self.args.conf,iou_thres=self.args.iou, task='detect')
+        self.seen = 0
+        self.jdict = []
+        self.stats = dict(tp=[], conf=[], pred_cls=[], target_cls=[], target_img=[])
+
+
 def ultralytics_val(args:Flags):
-    from ultralytics import YOLO
 
     # from pathlib import Path
     from datalabeling.train.utils import remove_label_cache
-
-    
-    
 
     # remove label.cache files
     remove_label_cache(data_config_yaml=args.data_config)
 
     for split in args.splits:
+
         print("-" * 20, split, "-" * 20)
-        print('\n',args.weights,'\n',args.data_config,)
-        model = YOLO(args.weights)
-        model.info()
+        
+        val_args = dict(model=args.weights, 
+                    data=args.data_config
+                )
+        
+        run_name = args.name + "#" + split + f"#{round(args.conf_threshold*100)}#{round(args.iou_threshold*100)}#{args.augment}#{args.max_det}-"
 
-        # Customize validation settings
-        name = args.name + "#" + split + f"#{round(args.conf_threshold*100)}#{round(args.iou_threshold*100)}#{args.augment}#{args.max_det}-"
-        model.val(
-            name=name,
-            project=args.project_name,
-            data=args.data_config,
-            imgsz=args.imgsz,
-            batch=args.batch_size,
-            split=split,
-            conf=args.conf_threshold,
-            iou=args.iou_threshold,
-            device=args.device,
-            single_cls=args.is_detector,
-            agnostic_nms=args.is_detector,
-            augment=args.augment,
-            save_conf=args.save_txt,
-            save_crop=False,
-            save_json=False,
-            plots=args.plots,
-            save_hybrid=args.save_hybrid,
-            save_txt=args.save_txt
-        )
+        validator = CustomValidator(args=val_args,save_dir=Path(args.project_name)/run_name)
+
+        # set args
+        validator.args.conf = args.conf_threshold
+        validator.args.iou = args.iou_threshold
+        validator.args.mode = 'val'
+        validator.args.imgsz = args.imgsz
+        validator.args.batch = args.batch_size
+        validator.args.device = args.device
+        validator.args.augment = args.augment
+        validator.args.split = split
+        validator.args.name = run_name 
+        validator.args.project = args.project_name
+        validator.args.max_det = args.max_det
+        validator.args.save_crop = False
+        validator.args.save_json = False
+        validator.args.plots = args.plots
+        validator.args.save_hybridd = args.save_hybrid
+        validator.args.save_txt = args.save_txt
+        validator.args.save_conf = args.save_txt
+
+        # run evaluation
+        results = validator()
+    
+        cf_matrix = validator.confusion_matrix.matrix
+        labels = list(validator.names.values()) + ["background"]
+
+        for i,label in enumerate(labels + ["background"]):
+
+            if label == 'background':
+                break
+
+            tp = cf_matrix[i,i]
+            actual_positive = cf_matrix[:,i].sum()
+            predicted_positive  = cf_matrix[i,:].sum()
+            # fp = predicted_positive - tp
+            # fn = actual_positive - tp
+
+            precision = tp/(predicted_positive + 1e-8)
+            recall = tp/(actual_positive + 1e-8)
+            f1score = 2*precision*recall / (precision + recall + 1e-8)
+
+            results = dict(precision=round(precision,4), recall=round(recall,4), f1score=round(f1score,4))
+
+            print(f"results for {label} : ", results,end='\n')
 
 
-def herdnet_val():
+
+
+def herdnet_val(args:Flags):
     from datalabeling.train.herdnet import HerdnetData, HerdnetTrainer
     from datalabeling.arguments import Arguments
     import lightning as L