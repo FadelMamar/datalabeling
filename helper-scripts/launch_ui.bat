a80a29e71e6ab50d239a3cec3c29afe885daffca
fadel <fadel.seydou@gmail.com>
2025-05-08 16:20:04 +0200

    update

--- helper-scripts/launch_ui.bat
+++ helper-scripts/launch_ui.bat
@@ -4,6 +4,14 @@ call deactivate
 
 call .venv\Scripts\activate
 
-call streamlit run UI\app.py
+@REM call cd UI
+
+call set LABEL_STUDIO_API_KEY=4f3c25bad9334596c5b2c3b270a2d3105c8b5d4a
+call set LABEL_STUDIO_URL=http://localhost:8080
+
+@REM call set TRAINING_API_URL = ...
+@REM call set TRAINING_API_KEY = ...
+
+call uv run streamlit run UI/app.py
 
 call deactivate
