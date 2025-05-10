51a1cfe611fe89e1c25e3cda66fc404a25d7aa51
fadel <fadel.seydou@gmail.com>
2025-05-10 14:45:00 +0200

    update

--- helper-scripts/register_run_to_mlflow.bat
+++ helper-scripts/register_run_to_mlflow.bat
@@ -6,16 +6,19 @@ call helper-scripts\activate_label-backend_env.bat
 
 @REM --use-sliding-window adding this flag will enabled sahi inference
 
+call set MLFLOW_S3_ENDPOINT_URL=http://localhost:9000
+call set AWS_SECRET_ACCESS_KEY=minioadmin
+call set AWS_ACCESS_KEY_ID=minioadmin
 
-call uv run tools\register_model.py --exp-name "labeler" --model "runs/mlflow/140168774036374062/685cbdce2d9b42658fa3ecb19c48261c/artifacts/weights/best.pt"^
-        --model-name "obb-detector" --mlflow-tracking-uri "http://localhost:5000"^
-        --tilesize 800 --imgsz 800 --confidence-threshold 0.1 --overlap-ratio 0.15^
-        --is-yolo-obb --use-sliding-window
+@REM call uv run tools\register_model.py --exp-name "labeler" --model "D:\datalabeling\base_models_weights\best.pt"^
+@REM         --model-name "labeler" --mlflow-tracking-uri "http://localhost:5000"^
+@REM         --tilesize 800 --imgsz 800 --confidence-threshold 0.1 --overlap-ratio 0.15^
+@REM         --use-sliding-window
 
 @REM call uv run tools\register_model.py --exp-name "labeler" --model ""^
 @REM         --model-name "detector" --mlflow-tracking-uri "http://localhost:5000"^
 @REM         --tilesize 800 --imgsz 800 --confidence-threshold 0.1 --overlap-ratio 0.15^
-@REM         --is-yolo-obb --use-sliding-window
+@REM         --use-sliding-window
 
 
 call deactivate
